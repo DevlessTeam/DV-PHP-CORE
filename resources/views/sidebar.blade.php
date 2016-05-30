d9a0df243fe1d7472c30d531cd0d1c431c2283c4
Edwin Tsatsu <paynegreen@yahoo.com>
2016-05-30 12:24:29 +0000

    icon changed

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -34,7 +34,7 @@
                     <li>
                         <h3 class="navigation-title">Menu<a href="{{url('logout')}}" class="pull-right">(Logout)</a></h3>
                     </li>
-                    <li><a href="{{route('app.index')}}"><i class="fa fa-code"></i><span>App</span></a></li>
+                    <li><a href="{{route('app.index')}}"><i class="fa fa-desktop"></i><span>App</span></a></li>
                     <li class="menu-list">
                         <a href="#"><i class="fa fa-gears"></i><span>Services</span></a>
                         <ul class="child-list">
