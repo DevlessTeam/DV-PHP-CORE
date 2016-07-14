8d4ca0c13ee26fd87a33d012f046210f0fbc5a75
eddymens <kwasiamantin@gmail.com>
2016-07-14 08:18:05 +0000

    fix allo allowed headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 header('Content-type','application/json');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PATCH, GET, POST, DELETE");
-header('Access-Control-Allow-Headers:  devless-token, devless-key, devless-user-token ');
+header('Access-Control-Allow-Headers:  Content-type, devless-token, devless-key, devless-user-token ');
  /*
 |--------------------------------------------------------------------------
 | Create The Application
