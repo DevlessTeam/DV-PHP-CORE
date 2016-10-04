1e8379271273788e76b7bc739ec58d8f47b2b409
Devless <devlessco@gmail.com>
2016-10-04 14:31:41 +0000

    fix db config

--- config/database.php
+++ config/database.php
@@ -66,18 +66,16 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-
-            'host'      => '127.0.0.1',//env('DB_HOST', 'localhost'),
-            'database'  => 'devless_production',
-            'username'  => 'root',
-            'password'  => 'password',
-
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
         ],
-
+        
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', 'localhost'),
