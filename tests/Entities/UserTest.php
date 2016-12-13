17db3e75737e33d0ac30997b450ebec14b3ba917
ntimobedyeboah <ntimyeboah.obed@callenssolutions.com>
2016-12-13 23:57:51 +0000

    Modify user class

---
+++ tests/Entities/UserTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserTest extends TestCase
+{
+    /**
+     * @var User
+     */
+    private $users;
+
+    /**
+     * Test setup
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->artisan('migrate');
+
+        $this->users = factory(User::class, 10)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_the_count_of_users_created()
+    {
+        $users = User::all();
+
+        $this->assertCount(10, $users);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_return_the_casted_role_and_status()
+    {
+        $this->assertEquals($this->users->first()->role, true);
+        $this->assertEquals($this->users->last()->role, true);
+        $this->assertEquals($this->users->first()->status, false);
+        $this->assertEquals($this->users->last()->status, false);
+    }
+
+    /**
+     * @test
+     */
+    public function it_checks_admin_role()
+    {
+        $user = factory(User::class)->create(['role' => 0]);
+
+        $this->assertTrue($this->users->first()->isAdmin());
+        $this->assertFalse($user->isAdmin());
+    }
+}
