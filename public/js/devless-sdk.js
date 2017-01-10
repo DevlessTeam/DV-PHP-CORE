dd67bfa6fc31ee661da8adc90f5b79391708ee30
eddymens <kwasiamantin@gmail.com>
2017-01-10 16:13:32 +0000

    minor devless sdk fixes

--- public/js/devless-sdk.js
+++ public/js/devless-sdk.js
@@ -10,8 +10,6 @@ var _jql = $;
 $.noConflict();
 function assignLable(){num=Math.round(100*Math.random())+1;var a=window.adjs,b=window.nouns;return adj=a[Math.floor(Math.random()*a.length)],noun=b[Math.floor(Math.random()*b.length)],separator="-",adj+separator+noun+separator+num}adjs="autumn hidden bitter misty silent empty dry dark summer icy delicate quiet white cool spring winter patient twilight dawn crimson wispy weathered blue billowing broken cold damp falling frosty green long late lingering bold little morning muddy old red rough still small sparkling throbbing shy wandering withered wild black young holy solitary fragrant aged snowy proud floral restless divine polished ancient purple lively nameless".split(" "),nouns="waterfall river breeze moon rain wind sea morning snow lake sunset pine shadow leaf dawn glitter forest hill cloud meadow sun glade bird brook butterfly bush dew dust field fire flower firefly feather grass haze mountain night pond darkness snowflake silence sound sky shape surf thunder violet water wildflower wave water resonance sun wood dream cherry tree fog frost voice paper frog smoke star".split(" ");
 
-
-	Devless = {};
 /**
 @author Devless
 @version 0.01
