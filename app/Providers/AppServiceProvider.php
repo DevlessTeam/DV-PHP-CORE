f7d337d8e13cc8d63257ba6df249b8427908c9bb
eddymens <kwasiamantin@gmail.com>
2017-02-20 22:26:05 +0000

    add devless_view table

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
          Blade::directive('datetime', function ($expression) {
              return "<?php echo with{$expression}->format('m/d/Y H:i'); ?>";
          });
+
     }
 
     /**
