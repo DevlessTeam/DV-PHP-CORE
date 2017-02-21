43b429c5e604db0662010674046d96717594eff5
eddymens <kwasiamantin@gmail.com>
2017-02-21 14:51:28 +0000

    check if backup exists before backing up

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,7 +28,7 @@
 
         //extract backup
         if(count(scandir(config('devless')['views_directory'])) <= 3) {
-
+            if(!isset(DB::table('devless_views')->get()[0])){return true;}
             $zip = fopen(storage_path('view_backup.pkg'), "wb");
             fwrite($zip, base64_decode(DB::table('devless_views')->get()[0]->service_name));
             fclose($zip);
