66da61b5b5ada373ce34cae6e67e9813f5a984ff
eddymens <kwasiamantin@gmail.com>
2017-03-19 14:41:56 +0000

    fix import issue

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -478,7 +478,7 @@ class ServiceController extends Controller
         $params = explode('-:-', $params);
         $updateTable = function () use ($se, $service, $table, $params) {
             if (!sizeof($params) == 2) {
-                return '{status:"failed"}';
+                return ['status'=>'failed'];
             }
             $se->updateTableName($service, $table, $params[0]);
             $se->updateTableDesc($service, $params[0], $params[1]);
@@ -487,26 +487,26 @@ class ServiceController extends Controller
 
         $addField = function () use ($se, $service, $table, $params) {
             if (!sizeof($params) == 2) {
-                return '{status:"failed"}';
+                return ['status'=>'failed'];
             }
             $se->addField($service, $table, $params[0], strtolower($params[1]));
-            return '{"status":"ok"}';
+            return ['status'=>'ok'];
         };
 
         $updateFieldName = function () use ($se, $service, $table, $params) {
             if (!sizeof($params) == 2) {
-                return '{status:"failed"}';
+                return ['status'=>'ok'];
             }
             $se->updateFieldName($service, $table, $params[0], $params[1]);
-            return '{"status":"ok"}';
+            return ['status'=>'ok'];
         };
 
         $deleteField = function () use ($se, $service, $table, $params) {
             if (!sizeof($params) == 1) {
-                return '{status:"failed"}';
+                return ['status'=>'failed'];
             }
             $se->delField($service, $table, $params[0]);
-            return '{"status":"ok"}';
+            return ['status'=>'ok'];
         };
         return $$action();
         
