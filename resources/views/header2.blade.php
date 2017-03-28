781dae9c53ee2f8c97c21749a669050c126264f2
Devless <devlessco@gmail.com>
2017-03-28 18:29:05 +0000

    add proxima nova font

--- resources/views/header2.blade.php
+++ resources/views/header2.blade.php
@@ -14,6 +14,7 @@
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css" rel="stylesheet">
 	<link href="{{ Request::secure(Request::root()).'/css/custom.min.css' }}" rel="stylesheet">
+	<link href="{{ Request::secure(Request::root()).'/css/proxima-nova.css' }}" rel="stylesheet">
 
 	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]><script src="js/html5shiv.js"></script><script src="js/respond.min.js"></script><![endif]-->
