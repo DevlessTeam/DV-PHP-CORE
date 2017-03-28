b686b475ef67054c7891a1f67f0878cc66dc7fc7
Devless <devlessco@gmail.com>
2017-03-28 18:05:43 +0000

    add assets, minor edits for ND

--- resources/views/footer2.blade.php
+++ resources/views/footer2.blade.php
@@ -1,65 +1,22 @@
-<!--footer section start-->
-<footer>
-    <script src="https://cdn.smooch.io/smooch.min.js"></script>
-<script>
-Smooch.init({ appToken: '9wokwlxqcy4n953mn3l2zz9y7' });
-</script>
-    2016 &copy; Devless.
+			<!-- footer content -->
+			<footer>
+				<div class="pull-right">
+					Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+				</div>
+				<div class="clearfix"></div>
+			</footer>
+			<!-- /footer content -->
+		</div>
+	</div>
 
-</footer>
-<!--footer section end-->
+	<script src="{{ Request::secure(Request::root()).'/js/jquery-1.10.2.min.js' }}"></script>
+	<script src="{{ Request::secure(Request::root()).'/js/bootstrap.min.js' }}"></script>
+	<!-- FastClick -->
+	<script src="{{ Request::secure(Request::root()).'/js/fastclick.js' }}"></script>
+	<!-- NProgress -->
+	<script src="{{ Request::secure(Request::root()).'/js/nprogress.js' }}"></script>
 
-
-
-</div>
-<!-- body content end-->
-</section>
-
-<!-- Placed js at the end of the document so the pages load faster -->
-<script src="{{ Request::secure(Request::root()).'/js/jquery-1.10.2.min.js' }}"></script>
-<script src="{{ Request::secure(Request::root()).'/js/jquery-migrate.js' }}"></script>
-<script src="{{ Request::secure(Request::root()).'/js/bootstrap.min.js' }}"></script>
-<!--notification pan-->
-<script src="{{ Request::secure(Request::root()).'/js/modernizr.min.js' }}"></script>
-
-<!-- datatable -->
-<script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
-
-<!--Nice Scroll-->
-<script src="{{ Request::secure(Request::root()).'/js/jquery.nicescroll.js' }}" type="text/javascript"></script>
-
-<!--right slidebar-->
-<script src="{{ Request::secure(Request::root()).'/js/slidebars.min.js' }}"></script>
-
-<!--switchery-->
-<script src="{{ Request::secure(Request::root()).'/js/switchery/switchery.min.js' }}"></script>
-<script src="{{ Request::secure(Request::root()).'/js/switchery/switchery-init.js' }}"></script>
-
-<!--Sparkline Chart-->
-<script src="{{ Request::secure(Request::root()).'/js/sparkline/jquery.sparkline.js' }}"></script>
-<script src="{{ Request::secure(Request::root()).'/js/sparkline/sparkline-init.js' }}"></script>
-
-<!--Form Validation-->
-<script src="{{ Request::secure(Request::root()).'/js/bootstrap-validator.min.js' }}" type="text/javascript"></script>
-
-<!--Form Wizard-->
-<script src="{{ Request::secure(Request::root()).'/js/jquery.steps.min.js' }}" type="text/javascript"></script>
-<script src="{{ Request::secure(Request::root()).'/js/jquery.validate.min.js' }}" type="text/javascript"></script>
-
-<!--wizard initialization-->
-<script src="{{ Request::secure(Request::root()).'/js/wizard-init.js'}}" type="text/javascript"></script>
-
-
-<!--common scripts for all pages-->
-<script src="{{ Request::secure(Request::root()).'/js/scripts.js' }}"></script>
-<!-- Ace Editor -->
-@if(\Request::path() != 'console')
-<script src="{{ Request::secure(Request::root()).'/js/ace/ace.js' }}" type="text/javascript" ></script>
-<script src="{{ Request::secure(Request::root()).'/js/ace/theme-github.js' }}" type="text/javascript" ></script>
-<script src="{{ Request::secure(Request::root()).'/js/ace/mode-php.js' }}" type="text/javascript" ></script>
-<script src="{{ Request::secure(Request::root()).'/js/ace/jquery-ace.min.js' }}" type="text/javascript" ></script>
-@endif
-@include('notifier')
+	<!-- Custom Theme Scripts -->
+	<script src="{{ Request::secure(Request::root()).'/js/custom.min.js' }}"></script>
 </body>
-
 </html>
--- resources/views/header2.blade.php
+++ resources/views/header2.blade.php
@@ -1,37 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <meta name="description" content="DevLess is a backend as a service framework that provide developers an easier way to rollout their web and mobile platform ">
-    <meta name="author" content="DevLess">
-    <meta name="keyword" content="DevLess, opensource, BAAS, Backend as a service, robust, php, laravel ">
-    <link rel="shortcut icon" href="favico.png" type="image/png">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<meta name="description" content="DevLess is a backend as a service framework that provide developers an easier way to rollout their web and mobile platform ">
+	<meta name="author" content="DevLess">
+	<meta name="keyword" content="DevLess, opensource, BAAS, Backend as a service, robust, php, laravel ">
+	<link rel="shortcut icon" href="favico.png" type="image/png">
 
-    <title>DevLess - {{config('devless')['version']}}</title>
+	<title>DevLess - {{config('devless')['version']}}</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css" rel="stylesheet">
-    <link href="{{ Request::secure(Request::root()).'/css/custom.min.css' }}" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css" rel="stylesheet">
+	<link href="{{ Request::secure(Request::root()).'/css/custom.min.css' }}" rel="stylesheet">
 
-    @if( \Request::path() == 'privacy' || \Request::path() == 'datatable' || \Request::path() == 'services')
-      <link rel="stylesheet" href="{{ Request::secure(Request::root()).'/css/helper.css' }}" media="screen" title="no title" charset="utf-8">
-      <script src="{{ Request::secure(Request::root()).'/js/jquery-1.10.2.min.js' }}"></script>
-      <script src="{{ Request::secure(Request::root()).'/js/bootstrap.min.js' }}"></script>
-    @endif
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]><script src="js/html5shiv.js"></script><script src="js/respond.min.js"></script><![endif]-->
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]><script src="js/html5shiv.js"></script><script src="js/respond.min.js"></script><![endif]-->
 
+    <style type="text/css">
+        .logo-wrap img {
+		    height: 40px;
+		    margin-left: 10px;
+		    margin-top: 10px;
+		}       
+    </style>
+    
 </head>
-<body onload="init()" class="sticky-header">
-
-    <section>
-      @if(\Request::path() != '/' && \Request::path() != 'setup')
-        <div class="header-section">
-          <!--toggle button start-->
-          <a class="toggle-btn"><i class="fa fa-outdent"></i></a>
-          <!--toggle button end-->
-        </div>
-        <!-- header section end-->
-@endif
+<body class="nav-md footer_fixed">
+	<div class="container body">
+		<div class="main_container">
