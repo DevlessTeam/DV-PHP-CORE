2a43781047465020bece62378a08795944c68fff
eddymens <kwasiamantin@gmail.com>
2017-03-29 07:42:43 +0000

    fix config

--- resources/views/service_views/notify/lib/Config.php
+++ resources/views/service_views/notify/lib/Config.php
@@ -32,20 +32,20 @@ class Config
         
         $configTree = 
 
-        ['sms' => ['accountId' => ($smsConfig->account_id)?$smsConfig->account_id : '',
-            'authToken' => ($smsConfig->auth_token) ?$smsConfig->auth_token : '',
-            'senderNumber' => ($smsConfig->sender_number) ?$smsConfig->sender_number : ''],
+        ['sms' => ['accountId' => (isset($smsConfig->account_id))?$smsConfig->account_id : '',
+            'authToken' => (isset($smsConfig->auth_token)) ?$smsConfig->auth_token : '',
+            'senderNumber' => (isset($smsConfig->sender_number)) ?$smsConfig->sender_number : ''],
 
-            'email' => ['api_key' => ($emailConfig->api_key) ? $emailConfig->api_key : '',
-                'sender_name' =>  ($emailConfig->sender_name) ? $emailConfig->sender_name : '',
-                'sender_email' => ($emailConfig->sender_email) ? $emailConfig->sender_email : ''],
+            'email' => ['api_key' => (isset($emailConfig->api_key)) ? $emailConfig->api_key : '',
+                'sender_name' =>  (isset($emailConfig->sender_name)) ? $emailConfig->sender_name : '',
+                'sender_email' => (isset($emailConfig->sender_email)) ? $emailConfig->sender_email : ''],
 
-            'push' => ['app_id' =>  ($pushConfig->app_id) ? $pushConfig->app_id : '',
-                'app_key' => ($pushConfig->app_key) ? $pushConfig->app_key : '',
-                'app_secret' => ($pushConfig->app_secret) ? $pushConfig->app_secret : '',
-                'app_general_broadcast_channel' =>  ($pushConfig->broadcast_channel) ? $pushConfig->broadcast_channel : '' ,
-                'app_general_broadcast_event' => ($pushConfig->broadcast_event) ? $pushConfig->broadcast_event : '',
-                'app_option' => ['encrypted' => ($pushConfig->app_options) ? (bool)$pushConfig->app_options  : ''],
+            'push' => ['app_id' =>  (isset($pushConfig->app_id)) ? $pushConfig->app_id : '',
+                'app_key' => (isset($pushConfig->app_key)) ? $pushConfig->app_key : '',
+                'app_secret' => (isset($pushConfig->app_secret)) ? $pushConfig->app_secret : '',
+                'app_general_broadcast_channel' =>  (isset($pushConfig->broadcast_channel)) ? $pushConfig->broadcast_channel : '' ,
+                'app_general_broadcast_event' => (isset($pushConfig->broadcast_event)) ? $pushConfig->broadcast_event : '',
+                'app_option' => ['encrypted' => (isset($pushConfig->app_options)) ? (bool)$pushConfig->app_options  : ''],
 
             ],
         ];
