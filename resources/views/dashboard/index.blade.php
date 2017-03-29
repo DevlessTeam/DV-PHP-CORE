a2fe56236cf813f010f2c61d708258971cbb2a76
Devless <devlessco@gmail.com>
2017-03-29 15:03:24 +0000

    dashboard ui fix

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -2,7 +2,18 @@
 
 @section('content')
 
-{{-- Page title --}}
+<style type="text/css">
+	.tuts-div {
+		border: 1px solid #d9dee4;
+		padding: 35px 15px;
+		text-align: center;
+		border-radius: 3px;
+	}
+	.tuts-div i {
+	    font-size: 35px;
+	}
+</style>
+{{-- Page title & Title Right --}}
 <div class="page-title">
 	<div class="title_left">
 		<h3>Welcome to Devless</h3>
@@ -10,7 +21,7 @@
 
 	<div class="title_right">
 		<div class="col-md-5 col-sm-5 col-xs-12 pull-right title-btn">
-			<button class="btn btn-primary addon-btn pull-right" data-toggle="modal" data-target=".bs-example-modal-lg"><i class="fa fa-plug"></i>Connect to Application</button>
+			<button class="btn btn-primary addon-btn pull-right"><i class="fa fa-plug"></i>Connect to My App</button>
 		</div>
 	</div>
 </div>
@@ -18,20 +29,113 @@
 
 <div class="clearfix"></div>
 
-{{-- Content begins --}}
+{{-- Content --}}
 <div class="row">
-	<div class="col-md-12 col-sm-12 col-xs-12">
+	<div class="col-md-8 col-sm-8 col-xs-12">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="x_panel">
+					<div class="x_title">
+						<h2>New to Devless?</h2>
+						<ul class="nav navbar-right panel_toolbox">
+							<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+								<ul class="dropdown-menu" role="menu">
+									<li><a href="#">Settings 1</a>
+									</li>
+									<li><a href="#">Settings 2</a>
+									</li>
+								</ul>
+							</li>
+							<li><a class="close-link"><i class="fa fa-close"></i></a>
+							</li>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+					<div class="x_content">
+						<p>Learn the key concepts of DevLess by following our 5-minute step-by-step tutorial. You can choose between building a simple blog or an e-commerce storefront. We provide templates to make this super easy.</p>
+						<div class="row">
+							<div class="col-md-4">
+								<div class="tuts-div">
+									<i class="fa fa-close"></i><h4>Build a Blog</h4>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="tuts-div">
+									<i class="fa fa-close"></i><h4>Build a Blog</h4>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="tuts-div">
+									<i class="fa fa-close"></i><h4>Build a Blog</h4>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="x_panel">
+					<div class="x_title">
+						<h2>Daily active users</h2>
+						<ul class="nav navbar-right panel_toolbox">
+							<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+								<ul class="dropdown-menu" role="menu">
+									<li><a href="#">Settings 1</a>
+									</li>
+									<li><a href="#">Settings 2</a>
+									</li>
+								</ul>
+							</li>
+							<li><a class="close-link"><i class="fa fa-close"></i></a>
+							</li>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+					<div class="x_content">
+						Enter Content
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-4 col-xs-12">
 		<div class="x_panel">
 			<div class="x_title">
-				<h2>Dashboard</h2>
+				<h2>Recent Activities</h2>
+				<ul class="nav navbar-right panel_toolbox">
+					<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#">Settings 1</a>
+							</li>
+							<li><a href="#">Settings 2</a>
+							</li>
+						</ul>
+					</li>
+					<li><a class="close-link"><i class="fa fa-close"></i></a>
+					</li>
+				</ul>
 				<div class="clearfix"></div>
 			</div>
 			<div class="x_content">
-				Content goes here
+				Content Here
 			</div>
 		</div>
 	</div>
 </div>
+{{-- Content end --}}
 
 {{-- Modal --}}
 <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
