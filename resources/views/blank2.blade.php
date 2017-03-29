171653aa8df2513181e97e47a82ec8bec03fcad3
Devless <devlessco@gmail.com>
2017-03-29 03:49:44 +0000

    add button to title right

--- resources/views/blank2.blade.php
+++ resources/views/blank2.blade.php
@@ -3,17 +3,12 @@
 	<div class="">
 		<div class="page-title">
 			<div class="title_left">
-				<h3>Plain Page</h3>
+				<h3>Welcome to Devless</h3>
 			</div>
 
 			<div class="title_right">
-				<div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-					<div class="input-group">
-						<input type="text" class="form-control" placeholder="Search for...">
-						<span class="input-group-btn">
-							<button class="btn btn-default" type="button">Go!</button>
-						</span>
-					</div>
+				<div class="col-md-5 col-sm-5 col-xs-12 pull-right title-btn">
+					<button class="btn btn-info addon-btn pull-right"><i class="fa fa-plug"></i>Connect to My App</button>
 				</div>
 			</div>
 		</div>
@@ -24,22 +19,7 @@
 			<div class="col-md-12 col-sm-12 col-xs-12">
 				<div class="x_panel">
 					<div class="x_title">
-						<h2>Plain Page</h2>
-						<ul class="nav navbar-right panel_toolbox">
-							<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-							</li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-								<ul class="dropdown-menu" role="menu">
-									<li><a href="#">Settings 1</a>
-									</li>
-									<li><a href="#">Settings 2</a>
-									</li>
-								</ul>
-							</li>
-							<li><a class="close-link"><i class="fa fa-close"></i></a>
-							</li>
-						</ul>
+						<h2>Dashboard</h2>
 						<div class="clearfix"></div>
 					</div>
 					<div class="x_content">
--- resources/views/header2.blade.php
+++ resources/views/header2.blade.php
@@ -22,9 +22,55 @@
     <style type="text/css">
         .logo-wrap img {
 		    height: 40px;
-		    margin-left: 10px;
-		    margin-top: 10px;
-		}       
+		    margin-left: 15px;
+		    margin-top: 8px;
+		}
+		.nav_title {
+			background: #172d44;
+		}
+		h1, h2, h3, h4, h5, h6 {
+			color: #4e5b68;
+		    letter-spacing: -0.2px;
+			font-family: "Proxima Nova",Helvetica,Arial,sans-serif;
+		}
+		h3 {
+			font-size: 28px;
+		}
+		body .container.body .right_col {
+		    background: #f5f5f7;
+		}
+		.x_panel {
+		    background-color: white;
+		    border: 1px solid rgba(43, 50, 57, 0.15);
+		    border-radius: 3px;
+		    margin-bottom: 40px;
+		}
+		.page-title .title_left h3 {
+			margin-top: 15px;
+		}
+		.addon-btn i.pull-right {
+    border-radius: 0 2px 2px 0;
+}
+
+.addon-btn i {
+    background-color: rgba(0, 0, 0, 0.1);
+    border-radius: 2px 0 0 2px;
+    height: 35px;
+    line-height: 35px;
+    margin: -6px 10px -6px -12px;
+    text-align: center;
+    width: 35px;
+}
+.title-btn {
+	padding: 0;
+}
+.page-title .title_right .title-btn {
+    margin: 0 0 10px;
+}
+.user-profile.dropdown-toggle {
+    padding-right: 25px;
+}
+
     </style>
     
 </head>
