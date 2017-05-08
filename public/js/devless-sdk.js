f4e015fd4b6d28c979265ab9ab17aa439f9e5cc2
eddymens <kwasiamantin@gmail.com>
2017-05-08 12:26:03 +0000

    ad notification to login/signup

--- public/js/devless-sdk.js
+++ public/js/devless-sdk.js
@@ -410,9 +410,11 @@
                 if( response.payload.result.message == undefined ) {
                     SDK.setToken(response.payload.result['token']);
                     devless_main.coreLib.notify("signup was successful");
+                    devless_main.coreLib.notify(response.message,1);
                     window.location.href = window.location.origin + '/' + actionUrl;
                 } else {
                     devless_main.coreLib.notify(response.payload.result.message)
+                    devless_main.coreLib.notify(response.message,0);
                 }
                 callback();
             });
@@ -429,9 +431,11 @@
                 if(response.payload.result !== false) {
                     SDK.setToken(response.payload.result['token']);
                     devless_main.coreLib.notify("Log in successfully");
+                    devless_main.coreLib.notify(response.message,1);
                     window.location.href = window.location.origin + '/' + actionUrl;
                 } else{
                     devless_main.coreLib.notify("Login failed");
+                    devless_main.coreLib.notify(response.message,0);
                 }
                 callback();
             });
@@ -491,9 +495,11 @@
             SDK.call('devless', 'logout', [], function(response){
                 if(response.payload.result == true) {
                     devless_main.coreLib.notify('Logout was successful')
+                    devless_main.coreLib.notify(response.message,1);
                     window.location.href = window.location.origin + '/' + actionUrl;
                 } else {
                     devless_main.coreLib.notify('Sorry could not log you out')
+                    devless_main.coreLib.notify(response.message,0);
                 }
             })
         }
