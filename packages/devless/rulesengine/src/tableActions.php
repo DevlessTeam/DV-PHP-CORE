fd41e7e46bee468df639334c7a2f05972e8a5805
eddymens <primerossgh@gmail.com>
2017-06-06 14:25:13 +0000

    add more adjectives to tableActions

--- packages/devless/rulesengine/src/tableActions.php
+++ packages/devless/rulesengine/src/tableActions.php
@@ -48,4 +48,41 @@ trait tableActions
 
         return $this;
     }
+    /**
+     * Check if table is being queried.
+     *
+     * @return $this
+     */
+    public function beforeQuerying()
+    {
+        return $this->onQuery();
+    }
+    /**
+     * Check if data is being added to table.
+     *
+     * @return $this
+     */
+    public function beforeCreating()
+    {
+        return $this->onCreate();
+    }
+    /**
+     * Check if data is being updated on table.
+     *
+     * @return $this
+     */
+    public function beforeUpdating()
+    {
+        return $this->onUpdate();
+    }
+    /**
+     * Check if data is being deleted from table.
+     *
+     * @return $this
+     */
+    public function beforeDeleting()
+    {
+        return $this->onDelete();
+    }
+
 }
