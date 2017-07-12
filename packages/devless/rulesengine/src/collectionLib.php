3a07e103ca7042ac8c85ba49b39fb3c36e515a1a
eddymens <kwasiamantin@gmail.com>
2017-07-12 19:15:45 +0000

    add collections to rules

--- packages/devless/rulesengine/src/collectionLib.php
+++ packages/devless/rulesengine/src/collectionLib.php
@@ -6,7 +6,64 @@ namespace Devless\RulesEngine;
 trait collectionLib
 {
 
-	//
+	public function fromTheCollectionOf($collection)
+	{
+		if (!$this->execOrNot) {
+            return $this;
+        }
+
+        $this->results = $collection;
+        return $this;
+	}
+
+	public function fetchAllWith($key, $value)
+	{
+		if (!$this->execOrNot) {
+            return $this;
+        }
+
+        
+		 $this->results = collect($this->results)->where($key, $value)->all();
+
+
+       
+
+        return $this;
+
+	}
+
+	public function fetchOnly($keys)
+	{
+		if (!$this->execOrNot) {
+            return $this;
+        }
+        if($this->isAssoc($this->results)){
+        	$this->results = collect($this->results)->only($keys)->all();
+		} else {
+			$newArray = [];
+			foreach($this->results as $singleObj){
+				$newArray[] = collect( (array)$singleObj )
+				->only($keys)->all();
+			}
+			$this->results = $newArray;
+		}
+        return $this;
+
+	}
+
+	public function isAssoc(array $arr)
+	{
+		if (!$this->execOrNot) {
+            return $this;
+        }
+
+	    if (array() === $arr) return false;
+	    return array_keys($arr) !== range(0, count($arr) - 1);
+	}
+
 		
 }
+
+
+
 
