6c674498de584700baaf6f72012cfdc5d0790c81
eddymens <kwasiamantin@gmail.com>
2017-08-11 09:15:03 +0000

    add phone number to all users method

--- packages/devless/systemClass/src/systemClass.php
+++ packages/devless/systemClass/src/systemClass.php
@@ -267,7 +267,7 @@ class devless
     {
         return DB::table('users')->select(
             [
-                "id", "username", "email", "first_name", "last_name", "status"
+                "id", "username", "email", "first_name", "last_name", "phone_number", "status"
             ]
         )->get();
     }
