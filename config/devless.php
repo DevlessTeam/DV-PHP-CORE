32229a91c5d9b9ceb85f85b3b0bf85be8d6efef2
eddymens <kwasiamantin@gmail.com>
2017-09-08 13:33:49 +0000

    bump version

--- config/devless.php
+++ config/devless.php
@@ -43,7 +43,7 @@ return[
   'name'                  => 'devless',   
   'views_directory_name'  => 'service_views',
   'views_directory'       => base_path().'/resources/views/service_views/',
-  'version'               => '1.3.1',
+  'version'               => '1.3.2',
 ];
 
 
