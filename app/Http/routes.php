b2caa90adf1d8d56a08960470745c0558b00fa6b
eddymens <kwasiamantin@gmail.com>
2017-09-15 09:39:42 +0000

    remove session-token for api calls

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,7 +31,8 @@
     Route::group(
         ['prefix' => 'api/v1', 'middleware' => 'cors'],
         function () {
-
+             Config::set('session.driver', 'array');
+             
             //check system status
             Route::get(
                 'status',
