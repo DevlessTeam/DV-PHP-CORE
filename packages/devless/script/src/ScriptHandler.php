9b2e34320dea9875b5ba886c4718a04c27d71b84
eddymens <kwasiamantin@gmail.com>
2017-10-18 08:56:36 +0000

    resolve bug

--- packages/devless/script/src/ScriptHandler.php
+++ packages/devless/script/src/ScriptHandler.php
@@ -64,6 +64,7 @@ class ScriptHandler
 $payload[script];
 EOT;
         $_____service_name = $payload['service_name'];
+        $_____init_vars = $payload['script_init_vars'];:
         $exec = function () use ($code, $rules, &$EVENT, $_____service_name, $_____init_vars, $payload) {   
 
             //store script params temporally
@@ -95,7 +96,7 @@ EOT;
             $footer  = '';
             $finalCode = (strpos($code, 'use App\Helpers\Assert')!==false)? $code : $headers.$code.$footer;
         
-        //    if($EVENT['request_phase'] == 'after'){extract($payload['ex_params'], EXTR_PREFIX_ALL, 'input');}
+            // if($EVENT['request_phase'] == 'after'){extract($payload['ex_params'], EXTR_PREFIX_ALL, 'input');}
         
             eval($finalCode);
 
