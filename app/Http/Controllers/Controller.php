de08db79ba8e9c204b40abb9eed0c2011db42aa4
Laravel Shift <shift@laravelshift.com>
2018-01-10 10:53:21 +0000

    Shift Authorization
    Laravel 5.1.11 introduces [Authorization][1]. This updates the app to
    use the new contracts, facades, traits, and providers.
    
    [1]: http://laravel.com/docs/5.1/authorization

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
