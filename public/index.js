6e4b7278c1abc4d47c782cf6bb7ad926c27675da
eddymens <kwasiamantin@gmail.com>
2018-01-24 15:04:09 +0000

    add service name to EVENT payload

---
+++ public/index.js
@@ -0,0 +1,44 @@
+Script.beforeQuerying = function () {
+};
+
+Script.beforeUpdating = function () {
+};
+
+Script.beforeDeleting = function () {
+}
+
+Script.beforeCreating = function () {
+}
+
+Script.onAnyRequest = function () {
+}
+
+Script.onCreate = function () {
+}
+
+Script.onUpdate = function () {
+}
+
+Script.onQuery = function () {
+}
+
+Script.onDelete = function () {
+}
+
+Script.afterCreating = function () {
+    EVENT.params.first_field = "Jacob";
+}
+
+Script.afterQuerying = function () {
+}
+
+Script.afterUpdating = function () {
+
+}
+
+Script.afterDeleting = function () {
+
+}
+
+
+;   
