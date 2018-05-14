6cf404ce9c2d4a3d5922f50c494b47a76ff686ee
Edwin Tsatsu <paynegreen@yahoo.com>
2018-05-14 13:04:29 +0000

    add tawk.to for support

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -44,3 +44,17 @@
 
 <!-- Toastr -->
 <script src="{{ Request::secure(Request::path()).'/js/toastr.js'}}" type="text/javascript"></script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5a2ea7e25d3202175d9b791f/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
