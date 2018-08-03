c05ee132a8a9ee7059903f66ecb6df12e3830ed7
eddymens <kwasiamantin@gmail.com>
2018-08-03 11:17:20 +0000

    add phpClass

---
+++ resources/views/service_views/PHPClass/view_assets/PHPClass/ActionClass.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * Created by Devless.
+ * Author: Eddymens
+ * Date Created: 11th of December 2017 12:29:17 PM
+ * Service: PHPClass
+ * Version: 1.3.5
+ */
+use App\Helpers\Helper;
+use App\Helpers\ActionClass;
+use App\Helpers\DataStore as ds;
+
+use PhpParser\Error;
+use PhpParser\NodeTraverser;
+use PhpParser\ParserFactory;
+use PhpParser\PrettyPrinter;
+use Devless\Script\Transverser;
+
+//Action method for serviceName
+class PHPClass
+{
+    public $serviceName = 'PHPClass';
+
+    
+    /**
+     * Methods decorated with `@ACL protected` are only available to users who are logged in . You may access the method via any of the SDKs.
+     * @ACL private
+     */
+    public function save($script)
+    {
+        $result = $this->compile_script($script);
+        if(!$result['successful']){
+            return $result['error_message'];
+        }
+        $script = preg_replace('/^<\?php(.*)(\?>)?$/s', '$1', $script);
+        $output = ds::service($this->serviceName, 'class')
+            ->where('id',1)->update(["script"=> $script]);
+        
+        return $output;
+    }
+
+
+    public function getCode()
+    {
+        $output = ds::service($this->serviceName, 'class')->getData();
+        return ($output['payload']['properties']['count'])?
+                '<?php '.addslashes($output['payload']['results'][0]->script):'<?php
+/** 
+* To use the PHP Class. 
+* First  import it into the rules of the service you want to use it
+* Then call on the execute method 
+* Eg 
+* ->import("PHPClass")
+* ->execute("sampleClass", "hello", [])
+**/
+
+class sampleClass {
+     public function hello()
+     {
+         return "HelloWorld"; 
+     }
+}
+';     
+        
+    }
+
+    /**
+     * Sample public method can be accessed by any user from any of the SDKs due to the `@ACL public decoration.
+     * @ACL private
+     */
+    public function execute($class, $method, $args=[])
+    {
+         $output = ds::service($this->serviceName, 'class')->getData();
+         if(!class_exists($class)) {
+            ($output['payload']['properties']['count'])?eval($output['payload']['results'][0]->script):"";         
+         }
+        
+        (strpos(error_get_last()['file'], 'ActionClass.php') !=false)?dd():'';
+
+        return (new $class())->{$method}(...$args);
+    }
+
+    /**
+     * List out all possible callbale methods as well as get docs on specific method 
+     * @param $methodToGetDocsFor
+     * @return $this;
+     */
+    public function help($methodToGetDocsFor=null)
+    {
+        $serviceInstance = $this;
+        $actionClass = new ActionClass();
+        return $actionClass->help($serviceInstance, $methodToGetDocsFor=null);   
+    }
+
+
+     public function compile_script($code)
+    {
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        $traverser = new NodeTraverser;
+        $traverser->addVisitor(new Transverser);
+        $prettyPrinter = new PrettyPrinter\Standard;
+        try {
+            $stmts = $parser->parse($code);
+            $stmts = $traverser->traverse($stmts);
+            $code = substr($prettyPrinter->prettyPrint($stmts), 6);
+        } catch (Error $e) {
+            $compiled_script['error_message'] = $e->getMessage();
+            $compiled_script['successful'] = false;
+            return $compiled_script;
+        }
+        
+        $compiled_script['script'] = $code;
+        $compiled_script['successful'] = true;
+        return $compiled_script;
+    }
+    /**
+     * This method will execute on service importation
+     * @ACL private
+     */
+    public function __onImport()
+    { 
+        ds::service($this->serviceName, 'class')->addData([["script"=> '
+/** 
+* To use the PHP Class. 
+* First  import it into the rules of the service you want to use it
+* Then call on the execute method 
+* Eg 
+* ->import("PHPClass")
+* ->execute("sampleClass", "hello", [])
+**/
+
+class sampleClass {
+     public function hello()
+     {
+         return "HelloWorld"; 
+     }
+}
+']]);
+        
+        //add code here
+
+
+    }
+
+
+    /**
+     * This method will execute on service exportation
+     * @ACL private
+     */
+    public function __onDelete()
+    {
+        //add code here
+
+    }
+
+
+}
+
+
+
+
---
+++ resources/views/service_views/PHPClass/view_assets/PHPClass/index.blade.php
@@ -0,0 +1,64 @@
+<?php 
+DvInclude($payload, 'ActionClass.php') ;
+$service = new $payload['service_name']();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+</head>
+
+<button class="btn btn-info pull-right" onclick="save()"><i class="far fa-save"></i> Save Code</button>
+<h2>PHPClasses</h2>
+<div style="color: green" class="text-center" id="message"></div>
+
+<body>
+
+<div id="container" style="width:100%;height:900px;border:1px solid grey"></div>
+
+<?=DVJSSDK()?>
+
+<script  src="https://unpkg.com/monaco-editor@0.10.1/min/vs/loader.js"></script>
+
+<script>
+  require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.13.1/min/vs' }});
+
+  require(['vs/editor/editor.main'], function() {
+    window.editor = monaco.editor.create(document.getElementById('container'), {
+      value:`<?=$service->getCode()?>`,
+      language: 'php'
+    });
+  });
+
+function save() {
+  var code = window.editor.getValue();
+  document.getElementById("message").style.color = "yellow";
+  document.getElementById("message").textContent = "Saving...";
+  SDK.call('PHPClass', 'save', [code], function(resp){
+     if(resp.payload.result.status_code == 619) {
+       document.getElementById("message").style.color = "green";
+       document.getElementById("message").textContent = "Saved";
+     }
+     else if (resp.payload.result.status_code == 629) {
+       document.getElementById("message").style.color = "green";
+       document.getElementById("message").textContent = "No new changes";
+     }else {
+        document.getElementById("message").style.color = "red";
+        document.getElementById("message").textContent = resp.payload.result; 
+     }
+  })
+}
+
+//save script
+document.addEventListener("keydown", function(e) {
+    if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+        e.preventDefault();
+        save();
+    }
+});
+</script>
+</body>
+</html>
